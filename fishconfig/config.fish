81c111623136925cb2955c6240ea8dd20c6641e7
S.Percentage <Syn.Tri.Naga@gmail.com>
2021-01-26 23:02:19 +0900

    add: gpg_tty and editor config

--- fishconfig/config.fish
+++ fishconfig/config.fish
@@ -1,4 +1,8 @@
 set -l ScriptPath (dirname (status -f))
+
+set -x GPG_TTY (tty)
+set -x EDITOR (which vim)
+
 source $ScriptPath/prompt.fish
 source $ScriptPath/local-aliases.fish
 source $ScriptPath/local-path.fish
