04ed9721160cdd61ada957ea32866d4e60fdb510
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2024-09-10 04:56:44 +0000

    add: home-manager program configs

--- nixos/home.wsl.nix
+++ nixos/home.wsl.nix
@@ -8,5 +8,27 @@
   programs.home-manager.enable = true;
   programs.git = {
     enable = true;
+    
+    userName = "S.Percentage";
+    userEmail = "2141528+Pctg-x8@users.noreply.github.com";
+
+    aliases = {
+      a = "add -p";
+      aa = "add";
+      cm = "commit -m";
+      st = "status -s";
+      co = "checkout";
+      p = "push";
+      pu = "push -u";
+      pff = "pull --ff-only";
+      merc = "merge --no-ff";
+      mcont = "merge --continue";
+      rh = "reset --hard";
+      lg1 = "log --oneline";
+    };
+  };
+  programs.neovim = {
+    enable = true;
   };
 }
+
